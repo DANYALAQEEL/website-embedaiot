4cc13590c9901f81f7c6b16b8dd5f9548bec8351
shahzadyounis <m.shahzad.younis@gmail.com>
2026-06-20 18:31:57 +0500

    fix: conditionally hide global header/footer/whatsapp float on admin route

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,14 +1,18 @@
+import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import WhatsAppButton from "../components/WhatsAppButton";
 
 export default function MainLayout({ children }) {
+    const location = useLocation();
+    const isAdmin = location.pathname.startsWith("/admin");
+
     return (
         <>
-            <Navbar />
+            {!isAdmin && <Navbar />}
             {children}
-            <Footer />
-            <WhatsAppButton />
+            {!isAdmin && <Footer />}
+            {!isAdmin && <WhatsAppButton />}
         </>
     );
 }
